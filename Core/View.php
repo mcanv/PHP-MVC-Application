2e3de74edf313c7c564be244672ca3df0c81f878
mcanv <mehmetcanvardar49@gmail.com>
2021-12-25 14:55:42 +0300

    Crate View.php file

--- Core/View.php
+++ Core/View.php
@@ -6,6 +6,11 @@ use Jenssegers\Blade\Blade;
 
 class View
 {
+    /**
+     * @var View
+     */
+    public View $view;
+
     public static function show($view, $data = []): string
     {
         $blade = new Blade(dirname(__DIR__) . '/public/views', dirname(__DIR__) . '/public/cache');
